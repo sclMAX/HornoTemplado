e928eb50ef57d25f23526ad2329dc3009cbce718
sclMAX <maxisoftman@gmail.com>
2017-03-02 19:15:28 -0300

    Desarrollo

--- HornoTemplado/HornoTemplado.ino
+++ HornoTemplado/HornoTemplado.ino
@@ -1,29 +1,17 @@
 #include <LiquidCrystal.h>
+#include "Switch.h"
 #include "config.h"
 #include "PT100.h"
 
-PT100 st(PIN_SENSOR);
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //    ( RS, EN, d4, d5, d6, d7)
-byte grado[8] =
-    {
-        0b00001100, // Los definimos como binarios 0bxxxxxxx
-        0b00010010,
-        0b00010010,
-        0b00001100,
-        0b00000000,
-        0b00000000,
-        0b00000000,
-        0b00000000};
-byte up[8] =
-    {
-        0B00100,
-        0B01110,
-        0B11111,
-        0B00100,
-        0B00100,
-        0B00100,
-        0B00100,
-        0B00000};
+PT100 st(PIN_SENSOR);                      //Inicializar sensor PT100
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7); //Inicializar Display
+Switch btnUp(PIN_UP, INPUT);
+Switch btnDown(PIN_DOWN);
+Switch btnMove(PIN_MOVE);
+Switch btnSet(PIN_SET);
+Switch btnOn(PIN_ON);
+Switch btnError(PIN_QERROR);
+
 volatile float Temp = 0;
 int TempladoMinTotal = 10 * 60;
 int TempladoMinActual = 0;
@@ -34,6 +22,8 @@ bool isCiclo = true;
 bool isOn = true;
 bool isOnCiclo = false;
 bool isCalentando = false;
+unsigned long lastUpdateTime = 0;
+bool updateNow = false;
 
 void setup()
 {
@@ -42,44 +32,14 @@ void setup()
   lcd.createChar(2, up);
   pinMode(PIN_QUEMADOR, OUTPUT);
   pinMode(PIN_RESET, OUTPUT);
-  pinMode(PIN_DOWN, INPUT);
-  pinMode(PIN_MOVE, INPUT);
-  pinMode(PIN_ON, INPUT);
-  pinMode(PIN_QERROR, INPUT);
-  pinMode(PIN_SET, INPUT);
-  pinMode(PIN_UP,INPUT);
 }
 
 void loop()
 {
+  readButtons();
   manageTemperatura();
   manageCiclo();
-  readButtons();
   updateLcd();
-  delay(1000);
-}
-
-void updateLcd()
-{
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  String temperaturas = "Te=";
-  temperaturas.concat(int(Temp));
-  temperaturas.concat("/");
-  temperaturas.concat(TempladoTemp);
-  lcd.print(temperaturas);
-  lcd.write(1);
-  if (isCalentando)
-  {
-    lcd.write(2);
-  }
-  lcd.setCursor(0, 1);
-  String tiempos = "Ti=";
-  tiempos.concat(String(TempladoMinActual / 60) + ":" + String(TempladoMinActual % 60));
-  tiempos.concat("-");
-  tiempos.concat(String(TempladoMinTotal / 60) + ":" + String(TempladoMinTotal % 60));
-  tiempos.concat(" h:m");
-  lcd.print(tiempos);
 }
 
 void manageTemperatura()
@@ -115,4 +75,47 @@ void manageCiclo()
 
 void readButtons()
 {
+  btnUp.poll();
+  if (btnUp.pushed())
+  {
+    lcd.setCursor(0, 10);
+    lcd.print("1");
+    TempladoTemp++;
+    updateNow = true;
+  };
+  btnDown.poll();
+  if (btnDown.pushed() && TempladoTemp > 10)
+  {
+    TempladoTemp--;
+    updateNow = true;
+  };
+}
+
+void updateLcd()
+{
+  if (((millis() - lastUpdateTime) > LCD_UPDATE_TIME)|| updateNow)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    String temperaturas = "Te=";
+    temperaturas.concat(int(Temp));
+    temperaturas.concat("/");
+    temperaturas.concat(TempladoTemp);
+    lcd.print(temperaturas);
+    lcd.write(1);
+    if (isCalentando)
+    {
+      lcd.write(2);
+    }
+    if(digitalRead(PIN_UP)) lcd.write(2);
+    lcd.setCursor(0, 1);
+    String tiempos = "Ti=";
+    tiempos.concat(String(TempladoMinActual / 60) + ":" + String(TempladoMinActual % 60));
+    tiempos.concat("-");
+    tiempos.concat(String(TempladoMinTotal / 60) + ":" + String(TempladoMinTotal % 60));
+    tiempos.concat(" h:m");
+    lcd.print(tiempos);
+    lastUpdateTime = millis();
+    updateNow = false;
+  }
 }
